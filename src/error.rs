cd2565dbf435fd027d2a656101ca7f854ead0c68
wennhmann <info@wennhmann.de>
2025-02-19 16:45:31 +0100

    init

--- src/error.rs
+++
@@ -1,2 +0,0 @@
-pub type Result<T> = core::result::Result<T, Error>;
-pub type Error = Box<dyn std::error::Error>; // For early dev.
--- src/splitter/mod.rs
+++
@@ -1,9 +0,0 @@
-// region:    --- Modules
-
-mod char_splitter;
-mod word_spliter;
-
-pub use char_splitter::*;
-pub use word_spliter::*;
-
-// endregion: --- Modules
